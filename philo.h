c955c779a3dbd7cf797ebd0d030050678b194ef6
Tanguy Billon <tanguy_billon@hotmail.fr>
2021-08-10 10:32:14 +0200

    include libs in .h && init main.c

---
+++ philo.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tanguy <tanguy@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/10 10:27:52 by tanguy            #+#    #+#             */
+/*   Updated: 2021/08/10 10:30:54 by tanguy           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+#define PHILO_H
+
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <pthread.h>
+
+#endif
