6320e37c70ab766d0445d63e40fc202ff7336639
Tanguy Billon <tanguy_billon@hotmail.fr>
2021-08-16 10:47:40 +0200

    multiple files && erros check && init

--- init.c
+++ init.c
@@ -6,11 +6,28 @@
 /*   By: tanguy <tanguy@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/16 09:36:09 by tanguy            #+#    #+#             */
-/*   Updated: 2021/08/16 10:12:26 by tanguy           ###   ########.fr       */
+/*   Updated: 2021/08/16 10:38:39 by tanguy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <philo.h>
+
+// initialisation des philosophers
+int init_philo(t_args *args)
+{
+    int i;
+
+    i = args->nb_of_philo;
+    while (i >= 0)
+    {
+        args->philo[i].id = i;
+        args->philo[i].amount_ate = 0;
+        args->philo[i].l_fork = i;
+        args->philo[i].r_fork = (i + 1) % args->nb_of_philo;
+        args->philo[i].last_meal = 0;
+        i--;
+    }
+}
 
 // initialisation de la strucutre principale au lancement du programme
 int init_struct(t_args *philo, char **av, int ac)
