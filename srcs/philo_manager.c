bd17ebb7b04c185aa381a55192966597335cfa95
Tanguy Billon <tanguy_billon@hotmail.fr>
2021-08-18 09:36:56 +0200

    prototype free philo

--- srcs/philo_manager.c
+++ srcs/philo_manager.c
@@ -6,8 +6,15 @@
 /*   By: tanguy <tanguy@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/18 08:48:42 by tanguy            #+#    #+#             */
-/*   Updated: 2021/08/18 08:48:46 by tanguy           ###   ########.fr       */
+/*   Updated: 2021/08/18 09:36:41 by tanguy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <philo.h>
+
+void    free_philo(t_data *data, t_philo *philo)
+{
+    // destroy mutex
+    // free data
+    // free philo 
+}
